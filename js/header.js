c89771aba48f7286284c19aa42f9aac016b5c512
Suraj <53539666+moonstoper@users.noreply.github.com>
2020-06-16 23:03:31 +0530

    added search,quotes

---
+++ js/header.js
@@ -0,0 +1,8 @@
+var description = new Array ();
+description[0] = "I can change";
+description[1] = "Isn't it cool";
+description[2] = "these are just to show you guys";
+description[3] = "another thing";
+var size = description.length
+var x = Math.floor(size*Math.random())
+document.write(description[x]);
